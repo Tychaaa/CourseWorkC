fda5aa242a339c6dfc68d25d8da76b9938511ad4
Timofey Tychkov <timofeytychkov@gmail.com>
2023-12-23 16:00:02 +0700

    Реализовал базовый класс InteractiveObject для будущего взаимодействия с объектами игрового мира

---
+++ InteractiveObject.cpp
@@ -0,0 +1,34 @@
+#include "InteractiveObject.h"
+
+// Конструктор
+InteractiveObject::InteractiveObject() : name(""), description("") {}
+
+// Конструктор с параметрами
+InteractiveObject::InteractiveObject(string& n, string& desc) : name(n), description(desc) {}
+
+// Деструктор
+InteractiveObject::~InteractiveObject() {}
+
+// Метод для получения имени объекта
+string InteractiveObject::getName()
+{
+	return name;
+}
+
+// Метод для получения описания объекта
+string InteractiveObject::getDescription()
+{
+	return description;
+}
+
+// Метод для установки имени объекта
+void InteractiveObject::setName(string& n)
+{
+	name = n;
+}
+
+// Метод для установки описания объекта
+void InteractiveObject::setDescription(string& desc)
+{
+	description = desc;
+}
---
+++ InteractiveObject.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Базовый класс для интерактивных объектов
+class InteractiveObject {
+
+private:
+
+    string name;             // Имя объекта
+    string description;      // Описание объекта
+
+public:
+
+    // Конструктор
+    InteractiveObject();
+    // Конструктор с параметрами
+    InteractiveObject(string& n, string& desc);
+    // Деструктор
+    ~InteractiveObject();
+
+    // Геттеры
+    string getName();
+    string getDescription();
+
+    // Сеттеры
+    void setName(string& n);
+    void setDescription(string& desc);
+
+    // Виртуальная функция для взаимодействия
+    virtual void interact() = 0;  
+};
